64b80493846db3f59ef1e1ba64634ca91cf366a8
Tiffany Kisiero <tifftawarar@gmail.com>
2019-12-13 19:27:52 +0300

    Add files via upload

---
+++ desire.js
@@ -0,0 +1,20 @@
+var sidebar = document.getElementById('sidebar');
+		var toggleButton = document.getElementById('toggle-icon')
+		toggleButton.addEventListener('click',function(){
+			if(sidebar.classList.contains('show')){
+				sidebar.classList.remove('show')
+				sidebar.classList.add('hide')
+			}
+			else{
+				sidebar.classList.add('show')
+				sidebar.classList.remove('hide')	
+			}
+			
+		})
+
+		$("button").click(function () {
+  $(".check-icon").hide();
+  setTimeout(function () {
+    $(".check-icon").show();
+  }, 10);
+});
